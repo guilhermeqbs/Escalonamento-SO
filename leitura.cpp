571ec1145fa4f7a7fad3e2b4a89e0801ad27e06d
Guilherme Basílio <guilhermeqbs@gmail.com>
2020-09-10 14:37:40 -0300

    leitura feita

--- leitura.cpp
+++ leitura.cpp
@@ -1,33 +1,55 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <locale.h>
 
 using namespace std;
 
+void entradaArquivo(string nomeTXT);
+
 int main(){
+    
+    setlocale(LC_ALL,NULL);
+
+    string nomeTXT="entrada.txt";
+
+    //Fazer o cin e cout bunitin.
+
+        entradaArquivo(nomeTXT);
+        
+    return 0;
+
+}
+
+void entradaArquivo(string nomeTXT){
 
     ifstream arquivo;
     string linha;
     int count=0;
 
-
-    arquivo.open("entrada.txt", ios::out );
-
-        //Rodar enquanto todo o arquivo não for lido(eof).
-        while(!arquivo.eof()) {
-            
-            //Quebra cada linha do arq e armazena numa string.
-            getline(arquivo,linha);
-            
-            cout <<"line: " <<linha<<endl;
-            cout <<"cont: "<<count<<endl<<endl;
-            
-            count++;
-
-            //Quebarar a linha nessas variaveis: (p,i,s).
-        }
-    
+    //Abri o arquivo.
+    arquivo.open(nomeTXT, ios::out );
         
-    return 0;
+        //Verifica se o arquivo foi aberto corretamente
+        if(arquivo.is_open()){
+            
+            //Rodar enquanto todo o arquivo não for lido(eof).
+            while(!arquivo.eof()) {
+                
+                //Quebra cada linha do arq e armazena numa string.
+                getline(arquivo,linha);
+                
+                cout <<"line: " <<linha<<endl;
+                cout <<"cont: " <<count<<endl<<endl;
+                
+                count++;
+
+            }
+        }
+        else
+        {
+            cout <<"\nO arquivo não foi aberto corretamente.\n";
+        }
+
 
 }
