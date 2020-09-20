631f40b8f177f4b0a64f58411d00c4ad8f24cf13
Guilherme Basílio <guilhermeqbs@gmail.com>
2020-09-19 21:51:31 -0300

    srt

--- main.cpp
+++ main.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include "fifo.cpp"
+#include "sjf.cpp"
 
 using namespace std;
 
-
 int main()
 {   
-    int y [3] = {3,14,20};
-    int s[3] = {33, 54, 42};
+    //int y [3] = {3,14,20};
+    //int s[3] = {33, 54, 42};
 
 
     //int p[3];
     //int y[3] = {3,5,6};
     //int s[3] = {5,6,4};
 
-    fifo(y,s);
+    //fifo(y,s);
+    
+    sjf(y,s);
+
 
     return 0;
 }
--- sjf.cpp
+++ sjf.cpp
@@ -3,6 +3,49 @@
 
 using namespace std;
 
-void sjf(){
+void sjf(int *y, int *s){
+    
+    int n = 3;
 
+    int espera[n]; //vetor que armazena o valor de espera de cada processo
+    int posicao = 0,tempoRes = 0;
+    float mediaEspera = 0, somaEspera = 0;
+    int x=18;
+   
+    //x = soma do valor s[] de todos + (primero y[0]).
+   
+    posicao = y[0];
+    //tempoRes = s[];
+    
+    int d=0;
+
+    for(int u=0; u<x; u++)//anda com a posicao 
+    {
+        for(int i=0; i<n; i++)//verificca se tem que inciar a verificao acada vez que a posicao anda
+        {
+            if(posicao == y[i])
+            {   
+                for(int j=0; j<i+1; j++)//comparar os valores de s dos processos na fila de espera
+                {
+                    if(s[i] < s[j])
+                    {
+                        d = i;
+                    }
+                }
+            }
+        }
+
+       s[d]--;
+        posicao++;
+    }
+
+    
+
+    /*
+    //Imprimi vetor
+     for(int i=0; i<n; i++)
+     {
+         cout <<espera[i]<<endl;
+     }
+     */
 }
