d3b7bd6aea32254a4d3f4d1dd5caea001ef3ce2e
Guilherme Basílio <guilhermeqbs@gmail.com>
2020-10-06 12:05:10 -0300

    mundacas

--- srt.cpp
+++ srt.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int menorNum(int *v, int n);
+int menorNum(int *v, int qtd);
 
-void srt(int n, int *y, int *s){
-
-    int d=0;//Posição do decrementador
-    int menorS = 0;//Menor valor de s[]
+void srt(int n, int *y, int *s)
+{
     int qtd = 0;//Quantidade que deve ser comparada -- Permite fazer as operações apenas dos processo que entraram. 
 
     int posicao=0;//Posição do processo
@@ -15,7 +14,6 @@ void srt(int n, int *y, int *s){
     
     int copiaS[n];//Copia do vetor s[]
     int auxS[n];//Copia do vetor s[]
-    
 
     int resposta[n];//Vetor que armazena o valor de resposta de cada posição
     int espera[n]; //Vetor que armazena o valor de espera de cada processo
@@ -32,7 +30,6 @@ void srt(int n, int *y, int *s){
         copiaS[p]= s[p];
         auxS[p] = s[p];
        
-        
         espera[p] = 0;
         resposta[p] = 0;
     }
@@ -43,50 +40,30 @@ void srt(int n, int *y, int *s){
     }
 
     posicao = y[0];//Determina onde o processo começa
-    
-    for(int u=0; u<tam; u++)//Movimenta a variável da posição 
-    {       
-            //Permite que apenas os processos que entraram executem
-            if(posicao <= y[n-1]){//Só executa nas entradas de processo
-                
-                qtd=-1;
 
-                for(int k=0; k<n; k++){
-                    
-                    if(posicao >= y[k])
-                    {
-                        qtd++;//Contador do número de execuções da variável i      
-                    }
+    for(int u=0; u<tam; u++)
+    {
+        //Permite que apenas os processos que entraram execute
+        if(posicao <= y[n-1])//Só executa nas entradas de processo
+        {
+            qtd=-1;
+
+            for(int k=0; k<n; k++){
+                
+                if(posicao >= y[k])
+                {
+                    qtd++;//Contador do número de execuções da variável i      
                 }
-            }
-
-        for(int i=0; i<=qtd; i++)//verifica se tem que inciar a verificação acada vez que a posição anda
-        {   
-              menorS = menorNum(s,qtd);
-            
-            for(int j=0; j<=i; j++)//comparar os valores de S dos processos na fila de espera
-            {   
-                
-                    //Trava s[i] e anda por todos s[j]. MenorS registra o primeiro s[] menor encontrado
-                    if( menorS > s[i] and s[i] < s[j] and s[i] > 0)// Não deixa o s[] ser menor do q 0.
-                    {                   
-                        {   
-                            menorS = s[i];
-                            d = i;// salva a posição quando tem q fazer a troca
-                                            
-                        }
-                    }
-                
             }
         }
 
         //registra os valores de s[] antes de andar a posição
         for(int i = 0; i<n; i++)
-        {   
+        {
             auxS[i] = s[i];   
         }
         
-        s[d]--;//Decrementa o valor de s[] do processo em execução.
+        s[menorNum(s,qtd)]--;//Decrementa o valor de s[] do processo em execução.
         posicao++;//Incrementa o valor da posição
       
         //Vetor dos valores do tempo de espera
@@ -127,23 +104,22 @@ void srt(int n, int *y, int *s){
         cout <<espera[i] <<"\t " <<resposta[i] <<endl;
     }
      
-    cout <<"SRT_: Media Espera: " <<mediaEspera <<endl;
+    cout <<"SRT_: Media Espera: " <<fixed <<setprecision(4) <<mediaEspera <<endl;
     
     cout <<"SRT_: Media Resposta: " <<mediaResposta;
-    
 }
 
-int menorNum(int *v, int qtd)
+int menorNum(int *v, int qtd)//retorna o indice do menor numero do vetor
 {   
     int menor=99999999;
+    int indice;
 
     for(int i=0;i<=qtd;i++)
     {  
         if (v[i] < menor and v[i] > 0)
         {
             menor = v[i];
+            indice = i;
         }
     }
-    //Subtrair o s[d] aqui??
-    return menor;
-}
+    return indice;
